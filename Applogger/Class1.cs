466e5df3dfc37e48da1a316773d2cb1872ba159a
daniainavilli <daniainavilli@gmail.com>
2021-09-30 10:55:35 +0530

    firtst commit

---
+++ Applogger/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Applogger
+{
+
+    public class Logger
+    {
+        public void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+    }
+}
